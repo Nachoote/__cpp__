efb58031a17e1eddfded1f6598d862f15397d980
nand_0 <fmpanizzads@gmail.com>
2019-07-15 13:45:55 -0300

    ejercicio 2 inicial

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -1,44 +1,102 @@
-/* Ejercicio 2
-Se desea implementar un juego, a partir de una matriz cuadrada de enteros de tamaño N.
-Para los ejemplos siguientes supondremos N = 5, pero el programa debe servir para cualquier valor
-de N.
-Inicialmente, el programa deberá cargar la matriz con enteros entre 0 y 9 solicitados al usuario,
-validando que no se ingrese otro valor fuera de este rango.
-Una vez cargada, debe mostrar la matriz por pantalla con formato de cuadrícula.
-Luego se solicita al usuario que indique cuantos intentos como máximo desea jugar.
-A continuación, en el desarrollo de cada intento:
-El programa generará un número al azar entre 0 y 9*N el cual se desplegará al usuario.
-o (Se debe investigar el procedimiento para producir números aleatorios).
-o Por ejemplo, para un valor de N = 5, el número al azar será un entero entre 0 y 45.
-Solicitar al usuario indique: número de fila, número de columna, diagonal (principal o
-secundaria) o la opción “ninguna”, que considere que la suma de sus elementos es igual al
-número obtenido al azar.
-En caso de que el usuario haya indicado alguna fila, columna o diagonal, verificar si la suma
-de sus elementos es igual al número obtenido al azar. De haber respondido “ninguna”,
-verificar que no exista efectivamente alguna fila, columna o diagonal que sume ese valor.
-Posterior a la validación se debe informar si el usuario acertó, o que su respuesta no es
-correcta mostrando el valor real de la suma de lo elegido por el usuario.
-En el caso que el usuario haya elegido que ninguna fila, columna o diagonal suma el valor,
-solamente indicar si la respuesta es correcta o no. Ante error, no es necesario indicar donde
-se cumple la suma.
-Se debe llevar un contador de la cantidad de aciertos y de fracasos del usuario, y darle la chance al
-usuario si quiere dejar de jugar (si viene perdiendo o se aburrió), independientemente que aún le
-queden intentos por realizar.
-La matriz se carga una vez sola al comienzo del juego, pero lo que irá cambiando es el valor al azar
-generado en cada uno de los intentos y siempre y cuando el usuario quiera seguir jugando. El juego
-terminará su ejecución cuando el usuario lo pida o cuando se llegue al último intento, lo que ocurra
-primero.
-Al finalizar, se le mostrará al jugador: el total de intentos, total de aciertos, total de fracasos, y un
-mensaje final de acuerdo con el siguiente criterio:
-* La cantidad de aciertos es contra la cantidad máxima de intentos
-Cantidad de aciertos
-Mensaje emitido
-0 a 1/3 de los intentos
-Hay que seguir practicando las sumas!
-Más de 1/3 de los intentos a 2/3 de los intentos
-No está mal, pero puede y debe mejorar.
-Más de 2/3 de los intentos al máximo de intentos-1
-Muy buen resultado
-Máximo de intentos
-Rendimiento impecable!
-*/
+#include <stdio.h>
+#include "./constantes.h"
+
+
+int main(){	
+	Matriz mat;
+	int i;
+	int j;
+	int valor;
+	int total;
+	int fil;
+	int col;
+	int diag;
+
+/* ====================== Cargar Matriz ========================*/
+	for(i = 0; i < EJ2_N; i++){
+		j = 0;
+		do{
+			printf("Ingresar valor para posicion %d,%d\n", i,j);
+			scanf("%d", &valor);
+			if(valor >= 0 && valor <= 9){
+				mat[i][j] = valor;
+				j++;
+			}
+			else{
+				printf("[Error]: El numero debe ser mayor que 0 y menor que 9\n");
+			}
+		}
+		while (j < EJ2_N);
+	}
+/* ====================== FIN Cargar Matriz ========================*/
+
+/* ====================== Imprimir Matriz ========================*/
+	for(i = 0; i < EJ2_N; i++){
+		printf("|");
+		for(j=0;j<EJ2_N;j++){
+			printf(" %d |",mat[i][j]);
+		}
+		printf("\n");
+	}
+/* ====================== FIN Imprimir Matriz ========================*/
+
+	
+	
+
+	
+/* ====================== Generar Numero Aleatorio ========================*/
+/* ====================== Fin Numero Aleatorio ========================*/
+
+	sumas sum;
+	printf("Ingresar linea a sumar: (F: fila, C: columna, D: diagonal):");
+	scanf("%d", &sum);
+	
+	total = 0;
+	switch(sum){
+		case 1:
+			/* ====================== Sumar Fila  ========================*/
+			printf("Ingresar numero de la fila a sumar:");
+			scanf("%d", &fil);
+			total = 0;
+			for(i=0;i<EJ2_N;i++){
+				total = total + mat[fil][i];
+			}
+			break;
+		case 2:
+			/* ====================== Sumar Columna  ========================*/
+			printf("Ingresar numero de la columna a sumar:");
+			scanf("%d", &col);
+			total = 0;
+			for(i=0;i<EJ2_N;i++){
+				total = total + mat[i][col];
+			}
+			break;
+		case 3:
+			printf("Ingresar numero de la diagonal a sumar:");
+			scanf("%d", &diag);
+			total = 0;
+			if(diag == 1){
+			/* ====================== Sumar Diagonal 1  ========================*/
+				for(i=0;i<EJ2_N;i++){
+					total = total + mat[i][i];
+				}
+			}
+			else { 
+				if(diag == 2){
+			/* ====================== Sumar Diagonal 2  ========================*/
+					for(i=EJ2_N -1;i>=0;i--){
+						total = total + mat[i][EJ2_N - i];
+					}
+				}
+				else {
+					printf("Las diagonales pueden ser 1 o 2\n");
+				}
+			}
+			break;
+		default:
+			printf("Opcion Incorrecta\n");
+			break;
+
+	}
+	printf("Total: %d\n", total);	
+}
