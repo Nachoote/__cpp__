6a96b14ee6ccaf1d53ffaf5f458c7af05d44d592
nand-0 <fmpanizzads@gmail.com>
2019-07-15 09:32:38 -0300

    Create constantes.h

---
+++ constantes.h
@@ -0,0 +1,10 @@
+// Constants.h
+#if !defined(MYLIB_CONSTANTS_H)
+#define MYLIB_CONSTANTS_H 1
+
+/* 
+Declarar constantes arriba del #endif
+esto despues se incluye a los ejercicios con #include <constantes.h>
+*/
+
+#endif
