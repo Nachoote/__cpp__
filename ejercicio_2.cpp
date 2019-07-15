1ff1a42cd4f10d90686eef60e95a3b0a8633c295
nand-0 <fmpanizzads@gmail.com>
2019-07-15 19:25:00 -0300

    Update ejercicio_2.cpp
    Agrego comentarios y lista de pendientes

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -1,21 +1,46 @@
-#include <stdio.h>
-#include "./constantes.h"
-#include <cstdlib>
-#include <ctime>
+#include <stdio.h>		// Libreria estandar de C, necesaria para el manejo de entrada y salida (printf y scanf)
+#include "./constantes.h"	// 
+#include <cstdlib>		// Libreria estandar de C, necesaria para la generacion de numeros aleatorios (rand)
+#include <ctime>		// Libreria para manejo de fechas y tiempo, necesaria para inicializar el generador de numeros aleatorios (time)
 
+
+
+
+/*======================================================
+ * 		POR HACER
+ * ====================================================
+ * 1 - Escribir el loop principal relativo a cantidad de intentos
+ * 2 - Escribir condiciones de acierto y no acierto
+ * 3 - Escribir el contador de puntaje y su manipulacion
+ * 4 - Normalizar los nombres de variables
+ *
+ * */
 
 int main(){	
-	Matriz mat;
-	int i;
-	int j;
-	int valor;
-	int total;
-	int fil;
-	int col;
-	int diag;
-	int random;
+/* ====================== Inicio ========================*/
+	Matriz mat; 		// Matriz de N * N
+	unsigned int i;		// Entero para recorrer array
+	unsigned int j;		// Entero para recorrer array
+	unsigned int valor;	// Entero para evaluar numero ingresado por el usuario en la carga de matriz
+	unsigned int total;	// Entero para guardar el total de la suma de fila, columna o diagonal  
+	unsigned int fil;	// Entero para seleccionar la fila que se quiere sumar
+	unsigned int col;	// Entero para indicar la columna que se quiere sumar
+	unsigned int diag;	// Entero para indicar diagonal principal o secundaria
+	unsigned int random;	// Entero para guardar numero aleatorio entre  0 y (9 * N)
+	unsigned int intentos;	// Entero para guardar numero de intentos que se desea jugar
+
+
+
+
+
 
 /* ====================== Cargar Matriz ========================*/
+/* 
+ * Para cada posicion en columna (i) y fila(j) solicito al usuario un numero y lo guardo en la variable valor.
+ * Si el numero es mayor que 0 y menor que 9 lo guardo en la posicion (i,j) y permito seguir la iteracion
+ * Si no, no avanzo la iteracion y despliego un mensaje indicando el error hasta que el numero ingresado sea valido
+ * 
+ */
 	for(i = 0; i < EJ2_N; i++){
 		j = 0;
 		do{
@@ -34,6 +59,11 @@ int main(){
 /* ====================== FIN Cargar Matriz ========================*/
 
 /* ====================== Imprimir Matriz ========================*/
+/*
+ * Para cada posicion de columna imprimo el valor guardado en la misma
+ * Una vez recorrida la fila imprimo un salto de linea para formar la cuadricula.
+ *
+ */
 	for(i = 0; i < EJ2_N; i++){
 		printf("|");
 		for(j=0;j<EJ2_N;j++){
@@ -43,15 +73,35 @@ int main(){
 	}
 /* ====================== FIN Imprimir Matriz ========================*/
 
+	printf("Ingrese cuantos intentos desea jugar:");
+	scanf("%d", &intentos);	
 	
-	
+
+
+
+
+/* ====================== Juego  ========================*/
 
 	
 /* ====================== Generar Numero Aleatorio ========================*/
+/*
+ * Primero inicializo el generador de numeros
+ * Despues utilizo la operacion modulo (%) para generar un numero entre 0 y (9 * N)
+ * Muestro el numero aleatorio
+ * */
 	srand(time(0));
-	random = (rand() % (9 * EJ2_N)) + 0;
+	random = (rand() % (9 * EJ2_N));
 	
-/* ====================== Fin Numero Aleatorio ========================*/
+/* ====================== Fin Numero Aleatorio ========================*/	
+
+	
+	
+/* ====================== Evaluacion de filas, columnas y diagonales ========================*/	
+/*
+ * Utilizo un tipo enumerado (sumas) para almacenar las posibles opciones  (F: fila, C: columna, D: diagonal)
+ * Pregunto al usuario si la suma de algun elemento es igual al numero aleatorio y que indique si el elemento es fila,columna o diagonal
+ * Despues pido que indique el numero de fila, columna o diagonal y lo guardo en la variable fil, col o diag dependiando del caso
+ **/
 
 	sumas sum;
 	printf("Ingresar linea a sumar: (F: fila, C: columna, D: diagonal):");
@@ -61,6 +111,10 @@ int main(){
 	switch(sum){
 		case 1:
 			/* ====================== Sumar Fila  ========================*/
+			/*Si es fila, pido el numero de fila y lo guardo en fil, 
+			 * despues recorro todas las posiciones de la fila (fil, i) y sumo el valor guardado 
+			 * en cada una a la variable total inicializada en 0
+			 * */
 			printf("Ingresar numero de la fila a sumar:");
 			scanf("%d", &fil);
 			total = 0;
@@ -70,6 +124,11 @@ int main(){
 			break;
 		case 2:
 			/* ====================== Sumar Columna  ========================*/
+			/*
+			 * Si es columna, pido el numero de columna y lo guardo en col,
+			 * despues recorro las posiciones de la columna (i, col) y sumo el valor guardado 
+			 * en cada una a la variable total inicializada en 0
+			 * */
 			printf("Ingresar numero de la columna a sumar:");
 			scanf("%d", &col);
 			total = 0;
@@ -83,6 +142,12 @@ int main(){
 			total = 0;
 			if(diag == 1){
 			/* ====================== Sumar Diagonal 1  ========================*/
+			/*
+			 * Para la diagonal principal inicio la variable i en 0 y la voy incrementando 
+			 * para recorrer las posiciones de la diagonal (i,i) y sumando su valor a la variable
+			 * total inicializada en 0
+			 *
+			 * */
 				for(i=0;i<EJ2_N;i++){
 					total = total + mat[i][i];
 				}
@@ -90,6 +155,13 @@ int main(){
 			else { 
 				if(diag == 2){
 			/* ====================== Sumar Diagonal 2  ========================*/
+			/*
+			 * Para la diagonal secundaria inicio en la posicion N en columnas y 0 en filas
+			 * para recorrer la diagonal voy reduciendo en 1 la variable i que recorre las columnas y 
+			 * aumentando el 1 el indice en columnas mediante la operacion N - i
+			 * para cada posicion recorrida sumo su valor a la variable total inicializada en 0
+			 *
+			 * */
 					for(i=EJ2_N -1;i>=0;i--){
 						total = total + mat[i][EJ2_N - i];
 					}
@@ -106,3 +178,4 @@ int main(){
 	}
 	printf("Total: %d\n", total);	
 }
+
