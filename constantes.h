6bc7f652be8c607245dec7c234fb8d8a309dddcd
nand-0 <fmpanizzads@gmail.com>
2019-07-16 15:16:10 -0300

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -1,11 +1,11 @@
-// Constants.h
 #if !defined(MYLIB_CONSTANTS_H)
 #define MYLIB_CONSTANTS_H 1
 
 const int EJ2_N = 3;
 
 typedef int Matriz[EJ2_N][EJ2_N];
-typedef enum {F=1,C,D} sumas;
+typedef enum {F=1,C,D,N} ej2_opcion;
+typedef enum {False, True} boolean;
 
 
 /* 
@@ -14,4 +14,3 @@ esto despues se incluye a los ejercicios con #include <constantes.h>
 */
 
 #endif
-
